96e300010d08c1f63bf5955abe0b88a731aaea57
Griessmeister <hgriessel2@gmail.com>
2022-04-19 09:07:33 +0000

    added .env file

--- mikenskietapp/CREATE DATABASE mikenskietdb;.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE mikenskietdb;
-ALTER ROLE mikenkietdb_user SET client_encoding TO 'utf8';
-ALTER ROLE mikenkietdb_user SET default_transaction_isolation TO 'read committed';
-ALTER ROLE mikenkietdb_user SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE mikenskietdb TO mikenkietdb_user;
--- mikenskietapp/settings.py
+++ mikenskietapp/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
+
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&a1u!ik7l1)t-8mm+u^rr4_y&z6oh(gtw(cv$h$7irjs0wc#nv'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,9 +93,9 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS=5000
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mikenskietdb',
-        'USER': 'mikenskiet_db_user',
-        'PASSWORD': '@1eenT0Tagt8@',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
         'HOST': 'localhost',
         'PORT': '',
     }
